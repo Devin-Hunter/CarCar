522cea804b20a2ceddad4e643a0b264985920944
Krystin <krystin.n.gonzales@gmail.com>
2024-02-05 16:37:58 -0800

    Navbar changed

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -2,15 +2,38 @@ import { NavLink } from 'react-router-dom';
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container-fluid">
         <NavLink className="navbar-brand" to="/">CarCar</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <div className="collapse navbar-collapse" id="navbarNavDarkDropdown">
+          <ul className='navbar-nav'>
+            <li className='nav-item dropdown'>
+              <a className="nav-link dropdown-toggle" id='navbarDarkDropdownMenuLink' role="button" data-bs-toggle="dropdown" aria-expanded="false" >Manufacturer</a>
+              <div className='dropdown-menu dropdown-menu-dark'>
+                <a className='dropdown-item' href='/'>Manufacturer List</a>
+                <a className='dropdown-item' href='/'>Add Manufacturer</a>
+              </div>
+            </li>
+            <li className='nav-item dropdown'>
+              <a className="nav-link dropdown-toggle" id='navbarDarkDropdownMenuLink' role="button" data-bs-toggle="dropdown" aria-expanded="false" >Models</a>
+              <div className='dropdown-menu dropdown-menu-dark'>
+                <a className='dropdown-item' href='/'>Model List</a>
+                <a className='dropdown-item' href='/'>Add Model</a>
+              </div>
+            </li>
+            <li className='nav-item dropdown'>
+              <a className="nav-link dropdown-toggle" id='navbarDarkDropdownMenuLink' role="button" data-bs-toggle="dropdown" aria-expanded="false" >Inventory</a>
+              <div className='dropdown-menu dropdown-menu-dark'>
+                <a className='dropdown-item' href='/'>Current Inventory</a>
+                <a className='dropdown-item' href='/'>Add to Inventory</a>
+              </div>
+            </li>
+
           </ul>
+            
         </div>
       </div>
     </nav>
